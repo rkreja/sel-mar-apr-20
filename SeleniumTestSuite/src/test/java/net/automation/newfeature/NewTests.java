5f44adcbff62ef6c8eb2ccf00e7f0e461a29d625
rkreja <rkrejanyc@yahoo.com>
2020-04-28 23:23:03 -0400

    test automated for new features

---
+++ SeleniumTestSuite/src/test/java/net/automation/newfeature/NewTests.java
@@ -0,0 +1,23 @@
+package net.automation.newfeature;
+
+import org.testng.annotations.Test;
+
+import net.timentraining.core.TestBase;
+
+public class NewTests extends TestBase {
+	@Test
+	public void newTest1() {
+		webui.launch("http://www.automationpractice.com");
+	}
+	
+	@Test
+	public void newTest2() {
+		webui.launch("http://www.automationpractice.com");
+	}
+	
+	@Test
+	public void newTest3() {
+		webui.launch("http://www.automationpractice.com");
+	}
+
+}
